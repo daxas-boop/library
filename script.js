647d01dc1eb43fccae6f065ed9bd65fa68a5fcb4
daxas-boop <daxas.swd@hotmail.com>
2020-08-15 01:30:36 -0300

    Validates the inputs. Finished render function

--- script.js
+++ script.js
@@ -4,43 +4,66 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.info = () => {
-        read === true ? read = "readed." : read = "not read yet."; 
-        return `${title} by ${author}, ${pages} pages, ${read} `
-    }
+    this.read = read === true ? 'Read' : 'Not read' 
+    this.toggleRead = () => { 
+        if(this.read === 'Read'){
+            this.read = 'Not Read'
+        } else {
+            this.read = 'Read'
+        }
+    };
+    
 }
 
 function addBookToLibrary() {
-    //gets inputs
     let title = document.querySelector('#title').value
     let author = document.querySelector('#author').value
     let pages = document.querySelector('#pages').value
     let read = document.querySelector('#read')
     read.checked ? read = true : read = false;
 
-    // creates a book object
     let newBook = new Book(title, author, pages, read);
-    // adds object to the "myLibrary" array
     myLibrary.push(newBook);
 }
 
 function render() {
-    // reset the container
-    // loops the array "myLibrary" 
-    // display each book on the container
+    // maybe add something to check if the container already has that item
     const $bookContainer = document.querySelector('#book-container')
     $bookContainer.innerHTML = '';
     myLibrary.map( item => {
         const $book = document.createElement('div');
         $book.classList.add('book');
+        $book.setAttribute('data-book', `${myLibrary.indexOf(item)}`)
+        const $deleteBook = document.createElement('button');
+        $deleteBook.classList.add('delete-book');
+        $deleteBook.innerText = 'X';
+        $deleteBook.addEventListener('click', e => {
+            deleteBook(e);
+            render(); // after deleting a book we want to set up all the dataset's to match the index fo the books in the array
+        });
         const $title = document.createElement('h2');
         $title.classList.add('title');
-        $title.innerText = item.title
-        $book.appendChild($title);
+        $title.innerText = item.title;
+        const $author = document.createElement('p');
+        $author.innerText = `Author: ${item.author}`;
+        $author.classList.add('author');
+        const $pages = document.createElement('p');
+        $pages.innerText = `Pages: ${item.pages}`;
+        $pages.classList.add('pages');
+        const $read = document.createElement('p');
+        $read.innerText = item.read;
+        $read.classList.add('read');
+        $read.addEventListener('click', e => {
+            item.toggleRead();
+            $read.innerText = item.read;
+        });
+        $book.appendChild($deleteBook);
+        $book.appendChild($title)
+        $book.appendChild($author)
+        $book.appendChild($pages)
+        $book.appendChild($read)
         $bookContainer.appendChild($book)
     });
-
 }
 
 function hideForm(){
@@ -54,43 +77,98 @@ function resetInputs(){
     document.querySelector('#read').checked = false;
 }
 
-const $newBook = document.querySelector('#new-book');
-$newBook.addEventListener('click', () => {
-    document.querySelector('#form').classList.remove('hidden');
-});
+function deleteBook(e) {
+    const $book = e.target.parentNode;
+    myLibrary.splice($book.dataset.book, 1);
+    $book.remove();
+}
 
-const $submit = document.querySelector('#submit');
-$submit.addEventListener('click', e => {
-    e.preventDefault();
-    // verify inputs
-    addBookToLibrary();
-    hideForm();
-    resetInputs();
-    render();
-});
+function newBook(){
+    const $newBook = document.querySelector('#new-book');
+    $newBook.addEventListener('click', () => {
+        document.querySelector('#form').classList.remove('hidden');
+    });
+}
 
+function verifyTitle(title) {
+    title = title.trim();
+    if(title.length > 40){
+        return 'The length of the title should be less than 30 characters.'
+    }
+    if(title === ''){
+        return 'The title should not be empty.'
+    }
+    return '';
+}
 
-// function verifyInputs(){
-//     let errorCounter = 0;
-//     const titleDisplay  = document.querySelector('#title')
-//     const authorDisplay = document.querySelector('#author')
-//     const pagesDisplay  = document.querySelector('#pages')
-//     // verify that the title has no more than 30 characters
-//     // show error
-//     if(title.length > 30) {
-//         errorCounter++
-//         titleDisplay.classList.add('error')
-//     }   
-//     // verify that the author has no more than 30 characters
-//     // show error
-//     if(author.length > 30) {
-//         errorCounter++
-//         authorDisplay.classList.add('error')
-//     } 
-//     // verify that the pages are no more than 22,400 pages(most pages in a book)
-//     // show error
-//     if(pages > 22400) {
-//         errorCounter++
-//         pagesDisplay.classList.add('error')
-//     }
-// }
+function verifyAuthor(author) {
+    author = author.trim();
+    if(author.length > 30){
+        return 'The length of the author should be less than 30 characters'
+    }
+    if(author === ''){
+        return 'The author should not be empty.'
+    }
+    return '';
+}
+
+function verifyPages(pages){
+    if(pages > 22400){
+        return 'The pages should not be longer than 22.400'
+    }
+    if(pages === ''){
+        return 'The pages should not be empty.'
+    }
+    return '';
+}
+
+function verifyInput(){
+    let title = document.querySelector('#title').value
+    let author = document.querySelector('#author').value
+    let pages = document.querySelector('#pages').value
+
+    let errorTitle = verifyTitle(title);
+    let errorAuthor = verifyAuthor(author);
+    let errorPages = verifyPages(pages);
+
+    let errors = {
+        title: errorTitle,
+        author: errorAuthor,
+        pages: errorPages
+    }
+
+    const noErrors = manageErrors(errors) === 0;
+    if(noErrors){
+        addBookToLibrary();
+        hideForm();
+        resetInputs();
+        render();
+    }
+}
+
+function manageErrors(errors) {
+    const $form = document.querySelector('#form');
+    let keys = Object.keys(errors);
+    let errorCounter = 0;
+    keys.forEach(key => {
+       let error = errors[key];
+       if(error){
+           errorCounter++
+           $form[key].classList.add('error');
+       }else {
+           $form[key].classList.remove('error');
+       }
+    });
+    return errorCounter;
+}
+
+function submitForm(){
+    const $submit = document.querySelector('#submit');
+    $submit.addEventListener('click', e => {
+        e.preventDefault();
+        verifyInput();
+    });
+}
+
+newBook();
+submitForm();
